cc70c2625e6e3dff4f9fcfb5d209297ba451ad02
anhek <shynn@icloud.com>
2019-01-10 11:40:44 +0100

    Ajout exercice WTF pyramid

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -20,7 +20,7 @@ end
 def full_pyramid
     block = "#"
     space = " "
-    i = 1
+    i = 0
 
     puts "Combien d'étages voulez-vous ?"
     print"> "
@@ -33,7 +33,77 @@ def full_pyramid
         puts (space * temp_space) + (block * temp_block)
         temp_space -= 1
         temp_block += 2
+        i+=1
     end
 end
 
-full_pyramid
+def reverse_pyramid
+    block = "#"
+    space = " "
+    i = 0
+
+    puts "Combien d'étages voulez-vous ?"
+    print"> "
+
+    nb = gets.chomp.to_i
+    temp_space = 0
+    temp_block = (nb * 2) - 1
+
+    while i < nb
+    puts (space * temp_space) + (block * temp_block) 
+    temp_block -= 2
+    temp_space += 1
+    i += 1
+    end
+end
+
+def diamond_full_pyramid(givenNumber)
+    block = "#"
+    space = " "
+    i = 0
+
+    nb = (givenNumber + 1)/2
+    temp_space = nb - 1
+    temp_block = 1
+
+    while i < nb
+        puts (space * temp_space) + (block * temp_block)
+        temp_space -= 1
+        temp_block += 2
+        i+=1
+    end
+end
+
+def diamond_reverse_pyramid(givenNumber)
+    block = "#"
+    space = " "
+    i = 0
+
+    nb = (givenNumber - 1)/2
+    temp_space = 1
+    temp_block = (nb * 2) - 1
+
+    while i < nb
+    puts (space * temp_space) + (block * temp_block) 
+    temp_block -= 2
+    temp_space += 1
+    i += 1
+    end
+end
+
+def wtf_pyramid
+    puts "Combien d'étages voulez-vous ?"
+    print"> "
+
+    nb = gets.chomp.to_i
+
+    if nb%2!=0
+        diamond_full_pyramid(nb)
+        diamond_reverse_pyramid(nb)
+    else 
+        puts "Désolé, ton nombre est pair"
+    end
+
+end 
+
+wtf_pyramid
